161770dd71a50d5fb5b813c881e7ebe4bbd3d201
Xanthus Wong <wong.xanthus@bcthk.com>
2021-12-01 10:14:00 +0800

    - add console project (need to separate them later)

---
+++ Camunda.Worker/Program.cs
@@ -0,0 +1,19 @@
+﻿using Camunda.Api.Client;
+using System;
+using System.Reflection;
+
+namespace Camunda.Worker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start the camunda client to listen the external task for the execution.");
+            var camundaEngineClient = new WorkerService("http://192.168.17.158:39090/engine-rest", Assembly.GetExecutingAssembly());
+            camundaEngineClient.StartupWithSingleThreadPolling();
+            Console.WriteLine("Press any key to stop the client");
+            Console.ReadKey();
+            camundaEngineClient.StopWorkerListener();
+        }
+    }
+}
